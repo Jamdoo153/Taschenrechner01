09018ddea2d59398c01563fd4a2c551b0f57b3e1
Michael Walke <Jamdoo@gmx.de>
2023-03-24 12:36:00 +0100

    GUI Stimmungsbarometer

---
+++ MyFristProject/StimmungBarometer.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyFristProject
+{
+    /// <summary>
+    /// Interaktionslogik für StimmungBarometer.xaml
+    /// </summary>
+    public partial class StimmungBarometer : Window
+    {
+        public StimmungBarometer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
