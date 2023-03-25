63d3812caa2aada13d3779670f05247982fac2cf
Michael Walke <Jamdoo@gmx.de>
2023-03-25 10:09:32 +0100

    GUI für Terminplaner erstellt

---
+++ MyFristProject/Terminplaner.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyFristProject
+{
+    /// <summary>
+    /// Interaktionslogik für Terminplaner.xaml
+    /// </summary>
+    public partial class Terminplaner : Window
+    {
+        public Terminplaner()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button1_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button1_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button2_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button2_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
