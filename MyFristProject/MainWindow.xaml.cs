493f5d5d6c875407f12cbdfd96a97500c9b9751f
Michael Walke <Jamdoo@gmx.de>
2023-03-26 12:24:13 +0200

    Läuft nicht

--- MyFristProject/MainWindow.xaml.cs
+++ MyFristProject/MainWindow.xaml.cs
@@ -26,79 +26,34 @@ namespace MyProject
             InitializeComponent();
         }
 
-        private void button1_Click1_1(object sender, RoutedEventArgs e)
+        private void Button1_Click1_1(object sender, RoutedEventArgs e)
         {
             TASCHENRECHNER taschenrechner = new TASCHENRECHNER();
             taschenrechner.Show();
         }
 
-        private void button2_Click1_1(object sender, RoutedEventArgs e)
+        private void Button2_Click1_1(object sender, RoutedEventArgs e)
         {
             Schätzenspiel schätzenspiel = new Schätzenspiel();
             schätzenspiel.Show();
         }
 
-        private void button3_Click1_1(object sender, RoutedEventArgs e)
+        private void Button3_Click1_1(object sender, RoutedEventArgs e)
         {
             Bildergalerie bildergalerie = new Bildergalerie();
             bildergalerie.Show();
         }
 
-        private void button4_Click1(object sender, RoutedEventArgs e)
+        private void Button4_Click1(object sender, RoutedEventArgs e)
         {
             StimmungBarometer stimmungBarometer = new StimmungBarometer();
             stimmungBarometer.Show();
         }
 
-        private void button5_Click1_1(object sender, RoutedEventArgs e)
+        private void Button5_Click1_1(object sender, RoutedEventArgs e)
         {
             Terminplaner terminplaner = new Terminplaner();
             terminplaner.Show();
-        }
-
-        private void button1_MouseEnter(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void button1_MouseLeave(object sender, MouseEventArgs e)
-        {
-
-        }
-        private void button2_MouseEnter(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void button2_MouseLeave(object sender, MouseEventArgs e)
-        {
-
-        }
-        private void button3_MouseEnter(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void button3_MouseLeave(object sender, MouseEventArgs e)
-        {
-
-        }
-        private void button4_MouseEnter_1(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void button4_MouseLeave_1(object sender, MouseEventArgs e)
-        {
-
-        }
-        private void button5_MouseEnter(object sender, MouseEventArgs e)
-        {
-
-        }
-        private void button5_MouseLeave(object sender, MouseEventArgs e)
-        {
-
         }
     }
 }
